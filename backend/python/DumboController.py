2e5d721e81d3487d05603280650605fbe5b90f3a
EvilGenius666 <evilgenius6@googlemail.com>
2014-04-22 18:44:48 +0100

    Removed unnecessary code from DumboControllers as befitting their purpose as extremely basic example controllers

--- backend/python/DumboController.py
+++ backend/python/DumboController.py
@@ -1,28 +1,10 @@
 #!/usr/bin/python
 from IRobot import IRobot
 from random import choice
-#import queue
-from numbers import Real
 
 class DumboController(object):
-    
     def controlRobot(self,robot):
-
-        # Select a random number
-
-        randno = choice(range(0,3))
-
-        # Convert this to a direction
-
-        if randno == 0:
-            direction = IRobot.LEFT
-        elif randno == 1:
-            direction = IRobot.RIGHT
-        elif randno == 2:
-            direction = IRobot.BEHIND
-        else:
-            direction = IRobot.AHEAD
-         
-        robot.face(direction)  # Face the robot in this direction */   
-
-        robot.advance()        # and move the robot */
+        # Select a random direction
+        direction = choice([IRobot.AHEAD, IRobot.RIGHT, IRobot.BEHIND, IRobot.LEFT])
+        robot.face(direction)  # Face the robot in this direction
+        robot.advance()        # and move the robot
